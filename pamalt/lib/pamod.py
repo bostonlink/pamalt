ab5aac7454a6c23357215d2426d79ec5d6a2f6bc
bostonlink <bostonlink@pentest-labs.org>
2012-12-25 21:12:25 -0500

    cleaned up pa module

--- pamalt/lib/pamod.py
+++ pamalt/lib/pamod.py
@@ -10,31 +10,26 @@ import urllib2, urllib
 import time
 import xml.etree.ElementTree as ET
 
+def http_get(full_url):
+    try:
+        req = urllib2.Request(full_url)
+        ret = urllib2.urlopen(req)
+        return ret.read()
+    except urllib2.HTTPError as e:
+        return e
+
 # Authenticates to PA API and returns API key for all API calls
 
 def pa_auth(pausr, papass, pa_hostname):
     
     base_url = 'https://%s/api/?' % pa_hostname
-    params_dic = {}
-    params_dic['type'] = 'keygen'
-    params_dic['user'] = pausr
-    params_dic['password'] = papass
+    params_dic = {'type': 'keygen', 'user': pausr, 'password': papass}
 
     enc_params = urllib.urlencode(params_dic)
     full_url = base_url + enc_params
-
-    try:
-
-        req = urllib2.Request(full_url)
-        ret = urllib2.urlopen(req)
-        ret_data = ret.read()
-	# Start parsing the returned XML
-	root = ET.fromstring(ret_data)
-	auth_key = root[0][0].text
-        return auth_key
-
-    except urllib2.HTTPError as e:
-	return e
+    ret_data = http_get(full_url)
+    root = ET.fromstring(ret_data)
+    return root[0][0].text
 
 # PA Dynamic report function, must provide a valid dynamic report name
 # See pa_dyn_rname.txt list for all valid report names
@@ -42,95 +37,41 @@ def pa_auth(pausr, papass, pa_hostname):
 def pa_dyn_report(pa_hostname, key, reportname, period=''):
     
     base_url = 'https://%s/api/?' % pa_hostname
-    params_dic = {}
-    params_dic['type'] = 'report'
-    params_dic['reporttype'] = 'dynamic'
-    params_dic['reportname'] = reportname
-    params_dic['period'] = period
-    params_dic['key'] = key
+    params_dic = {'type': 'report', 'reporttype': 'dynamic', 'reportname': reportname, 'period': period, 'key': key}
 
     enc_params = urllib.urlencode(params_dic)
     full_url = base_url + enc_params
-    
-    try:
-
-        req = urllib2.Request(full_url)
-        ret = urllib2.urlopen(req)
-        ret_data = ret.read()
-        return ret_data
-
-    except urllib2.HTTPError as e:
-        return e
+    return http_get(full_url)
 
 # PA predefined report function, must provide a valid predefined report name
 
 def pa_pred_report(pa_hostname, key, reportname):
 
     base_url = 'https://%s/api/?' % pa_hostname
-    params_dic = {}
-    params_dic['type'] = 'report'
-    params_dic['reporttype'] = 'predefined'
-    params_dic['reportname'] = reportname
-    params_dic['key'] = key
+    params_dic = {'type': 'report', 'reporttype', 'predefined', 'reportname': reportname, 'key': key}
 
     enc_params = urllib.urlencode(params_dic)
     full_url = base_url + enc_params
-
-    try:
-
-        req = urllib2.Request(full_url)
-        ret = urllib2.urlopen(req)
-        ret_data = ret.read()
-        return ret_data
-
-    except urllib2.HTTPError as e:
-        return e
+    return http_get(full_url)
 
 # PA log query function to query pa logs and return the results in XML format
 
 def pa_log_query(pa_hostname, key, log_type, query=''):
 
     base_url = 'https://%s/api/?' % pa_hostname
-    params_dic = {}
-    params_dic['type'] = 'log'
-    params_dic['logtype'] = log_type
-    params_dic['query'] = query
-    params_dic['key'] = key
+    params_dic = {'type': 'log', 'logtype': log_type, 'query': query, 'key': key}
     
     enc_params = urllib.urlencode(params_dic)
     full_url = base_url + enc_params
-
-    try:
-
-        req = urllib2.Request(full_url)
-        ret = urllib2.urlopen(req)
-        ret_data = ret.read()
-
-    except urllib2.HTTPError as e:
-        return e
-
+    ret_data = http_get(full_url)
     root = ET.fromstring(ret_data)
-    job = root[0][1].text
-    return job
+    return root[0][1].text
     
 def pa_log_get(pa_hostname, key, jobid):
 
     base_url = 'https://%s/api/?' % pa_hostname
-    params_dic = {}
-    params_dic['type'] = 'log'
-    params_dic['action'] = 'get'
-    params_dic['job-id'] = jobid
-    params_dic['key'] = key
+    params_dic = {'type': 'log', 'action': 'get', 'job-id': jobid, 'key': key}
 
     enc_params = urllib.urlencode(params_dic)
     full_url = base_url + enc_params
-
-    try:
-
-        req = urllib2.Request(full_url)
-        ret = urllib2.urlopen(req)
-        ret_data = ret.read()
-	return ret_data
-
-    except urllib2.HTTPError as e:
-        return e
+    return http_get(full_url)
